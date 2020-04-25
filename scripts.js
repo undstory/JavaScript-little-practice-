4ab9a41fad0f5b3c455898a21747d1f1c9368e9c
undstory <undrul.ozog@gmail.com>
2020-04-25 21:38:33 +0200

    zad3 finish

--- scripts.js
+++ scripts.js
@@ -6,22 +6,27 @@ const btn = document.querySelector('.get');
 const output1 = document.querySelector('.output1');
 const output2 = document.querySelector('.output2');
 const output3 = document.querySelector('.output3');
+const output4 = document.querySelector('.output4');
+const output5 = document.querySelector('.output5');
 
 const arr = [];
 
 function getNumber(){
 
     if(number.value !== '') {
-        let numero = number.value;
+        let numero = parseFloat(number.value);
         arr.push(numero);
         number.value = '';
-
+        number.focus();
     if(arr.length == 5){
 
         this.disabled = true;
         output1.append(arr);
         output2.append(arr.reverse());
         output3.append(arr.filter(value => value > 20));
+        output4.append(arr.reduce((a, b) => a + b));
+        output5.append(Math.max(...arr));
+        // inny zapis: (Math.max.apply(Math, arr));
     } 
     } else {
         alert('Wpisz liczbę!');
@@ -56,43 +61,39 @@ let weight = document.querySelector(".weight"),
     growth = document.querySelector(".growth"),
     bmi_result = document.querySelector(".bmi_result"),
     bmi_btn = document.querySelector(".bmi_btn"),
-    interpretation = document.querySelector(".interpretation");
-
-
+    interpretation = document.querySelector(".interpretation"),
+    bmi_btn_new = document.querySelector(".bmi_btn_new");
 
 function getBMI(){
 
-    if(growth.value !== '' & weight.value !== ''){
-        weight = weight.value; 
-        growth = growth.value/100; 
-        let bmi = weight/(growth*growth);
+    if(growth.value !== '' & growth.value > 0 & weight.value !== '' & weight.value > 0){
+        getweight = parseFloat(weight.value); 
+        getgrowth = parseFloat(growth.value/100);
+        let bmi = getweight/(getgrowth*getgrowth);
         bmi = bmi.toFixed(1);
         bmi_result.innerHTML = bmi;
+       
 
-        if(bmi < 15){
-        interpretation.innerHTML = "wygłodzenie"; 
-        } else if(bmi > 15 & bmi < 17.4) {
-            interpretation.innerHTML = "wychudzenie";
-        } else if(bmi >= 17.5 & bmi <= 18.5) {
-            interpretation.innerHTML = "niedowaga";
-        } else if(bmi >= 18.6 & bmi <= 24.9) {
+        if(bmi < 18){
+        interpretation.innerHTML = "niedowaga"; 
+        } else if(bmi > 18 && bmi < 25) {
             interpretation.innerHTML = "wartość prawidłowa";
-        } else if(bmi >= 25 & bmi <= 29.9) {
+        } else if( bmi >= 25) {
             interpretation.innerHTML = "nadwaga";
-        } else if(bmi >= 30 & bmi <= 34.9) {
-            interpretation.innerHTML = "I stopień otyłości";
-        } else if(bmi >= 35 & bmi <= 39.9) {
-            interpretation.innerHTML = "II stopień otyłości";
-        } else (bmi >= 40) 
-            interpretation.innerHTML = "II stopień otyłości";
+        }         
         
     } else {
         alert("Wprowadziłeś nieprawidłowe dane!")
-    }
+    }    
 } 
-
 
 bmi_btn.addEventListener("click", getBMI); 
 
+bmi_btn_new.addEventListener("click", function(){
+    growth.value = "";
+    weight.value = "";
+    bmi_result.value = "";
+    interpretation.value = ""; 
+})
 
 
